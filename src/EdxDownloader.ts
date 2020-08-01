67b3b4c43962ed701e9eabda356ac4421a6e9269
Laurence Ion <ionlaurentiucristian@gmail.com>
2020-08-01 20:27:33 +0300

    Revert "deprecating project"
    This reverts commit 88b2cb5515ca559ed826febe83023d00c4212fb9.

---
+++ src/EdxDownloader.ts
@@ -0,0 +1,109 @@
+import path = require('path')
+
+import { from } from 'rxjs'
+import { flatMap } from 'rxjs/operators'
+
+import { Downloader, DownloadTask, DownloadResult } from "./Core"
+import { getInnerText } from "./Utils"
+
+
+interface EdxDownloadTask extends DownloadTask {
+  url: string
+}
+
+interface EdxDownloadResult extends DownloadResult {
+  baseName: string
+}
+
+export class EdxDownloader extends Downloader {
+
+  static courseUrlPattern = /^https:\/\/courses.edx.org\/courses\/.*\/course\/$/
+
+  login = () => this.withPage('https://courses.edx.org/login', async page => {
+    await page.waitFor('#login-email')
+    await page.type('#login-email', this.configuration.user)
+    await page.type('#login-password', this.configuration.password)
+    const loginEndpoints = [
+      "https://courses.edx.org/user_api/v1/account/login_session/",
+      "https://courses.edx.org/login_ajax"
+    ]
+    const [response] = await Promise.all([
+      page.waitForResponse(r => loginEndpoints.includes(r.url())),
+      page.click('.login-button')
+    ])
+
+    if (response.status() !== 200) {
+      throw `Login failed. Login response status code: ${response.status()}`
+    }
+
+    this.cookies.push(...await page.cookies())
+  })
+
+  getDownloadTasks = () => this.withPage(this.configuration.courseUrl, async page => {
+    const tasks: DownloadTask[] = []
+
+    // @ts-ignore
+    const subsectionUrls = await page.$$eval("a.outline-button", es => es.map(e => e.href))
+    for (const subsectionUrl of subsectionUrls) {
+      await page.goto(subsectionUrl)
+
+      const subsectionPages = await page.$$eval("button.tab.nav-item", es => es.map(
+        e => window.location.protocol + "//" + window.location.host + "/" + window.location.pathname + $(e).data("element")
+      ))
+      for (const url of subsectionPages) {
+        tasks.push({ id: url, name: url, url: url, index: tasks.length } as EdxDownloadTask)
+      }
+    }
+
+    return tasks
+  }).pipe(
+    flatMap(ts => from(ts))
+  )
+
+  performDownload = (task: EdxDownloadTask) => this.withPage(task.url, async page => {
+    await page.waitFor("#seq_content")
+    await this.waitForRender(page)
+
+    const breadcrumbs = (await getInnerText(".breadcrumbs span", page)).filter(b => b !== "").slice(1)
+    const baseName = `${task.index + 1} - ${this.buildTitle(breadcrumbs)}`
+
+    await page.evaluate(prettifyPage)
+
+    await this.waitForRender(page)
+
+    await this.savePage(baseName, page)
+
+    return { task: task, baseName: baseName } as EdxDownloadResult
+  })
+
+  reportResults(results: DownloadResult[]) {
+    console.log(`\nSaved ${results.length} pages to: ${path.resolve(this.configuration.output)}`)
+  }
+
+}
+
+function prettifyPage() {
+  $(".show").trigger("click")
+  $(".hideshowbottom").trigger("click")
+  $(".close-modal").trigger("click")
+  $(".discussion-show.shown").trigger("click")
+  $(".submit-attempt-container").hide()
+  $(".discussion-module").hide()
+  $(".course-wrapper").css({ "border": "0px", "box-shadow": "none" })
+  $(".annotator-notice").css({ "border": "0px" })
+  $("header").hide()
+  $("#footer-edx-v3").hide()
+  $(".course-tabs").hide()
+  $(".course-expiration-message").hide()
+  $(".first-purchase-offer-banner").hide()
+  $(".verification-sock").hide()
+  $("#frontend-component-cookie-policy-banner").hide()
+  $(".sequence-bottom").hide()
+  $(".sequence-nav").hide()
+  $(".nav-utilities").hide()
+  $(".course-license").hide()
+  $(".bookmark-button-wrapper").hide()
+  $(".subtitles").hide()
+  $(".video-wrapper").hide()
+  $(".proctored_exam_status").hide()
+}
---
+++ src/index.ts
@@ -0,0 +1,186 @@
+#!/usr/bin/env node
+
+import prompt = require('async-prompt')
+import program = require('commander')
+
+import { of, from, Observable } from 'rxjs'
+import { toArray, mergeMap, flatMap, tap, shareReplay, filter, distinct } from 'rxjs/operators'
+import { retryBackoff } from "backoff-rxjs"
+
+import { Configuration, Downloader } from "./Core"
+import { clone } from "./Utils"
+import { EdxDownloader } from "./EdxDownloader"
+import { CourseraDownloader } from "./CourseraDownloader"
+
+
+const downloaders = [
+  {
+    platform: "Edx",
+    courseUrlPattern: EdxDownloader.courseUrlPattern,
+    concurrency: 4,
+    factory: (c: Configuration) => new EdxDownloader(c) as Downloader
+  },
+  {
+    platform: "Coursera",
+    courseUrlPattern: CourseraDownloader.courseUrlPattern,
+    concurrency: 1,
+    factory: (c: Configuration) => new CourseraDownloader(c) as Downloader
+  }
+]
+
+function getDownloader(courseUrl: string) {
+  const found = downloaders.find(d => courseUrl.match(d.courseUrlPattern))
+  if (!found) {
+    throw `Unable to find downloader for this course url: ${courseUrl}`
+  } else {
+    return found
+  }
+}
+
+async function getConfiguration(): Promise<Configuration> {
+  function parseFormat(value: string, _: string) {
+    if (!["pdf", "png"].includes(value)) {
+      console.log(`invalid format: ${value}`)
+      process.exit(1)
+    } else {
+      return value
+    }
+  }
+
+  function parseRange(value: string, _: string) {
+    const match = /^(\d+)-(\d+)$/.exec(value)
+    if (!match) {
+      console.log(`invalid range: ${value}`)
+      process.exit(1)
+    } else {
+      return { from: parseInt(match[1]), to: parseInt(match[2]) }
+    }
+  }
+
+  program
+    .name("edx-archive")
+    .arguments('<course_url>')
+    .option('-u, --user <email>', 'edx login (email)')
+    .option('-p, --password <password>', 'edx password')
+    .option('-o, --output <directory>', 'output directory', 'Archive')
+    .option('-f, --format <format>', 'save pages as pdf or png', parseFormat, 'pdf')
+    .option('--filter-range <range>', 'filter downloads by number using range, e.g. 1-5', parseRange, { from: 1, to: Infinity })
+    .option('-r, --retries <retries>', 'number of retry attempts in case of failure', v => parseInt(v), 3)
+    .option('-d, --delay <seconds>', 'delay before saving page', v => parseInt(v), 1)
+    .option('-c, --concurrency <number>', 'number of pages to save in parallel', v => parseInt(v), null)
+    .option('--no-headless', 'disable headless mode')
+    .option('--debug', 'output extra debugging', false)
+    .parse(process.argv)
+
+  if (program.args.length !== 1) {
+    program.help()
+  }
+
+  const configuration = clone(program.opts())
+
+  configuration.courseUrl = program.args[0]
+
+  if (configuration.concurrency === null) {
+    configuration.concurrency = getDownloader(configuration.courseUrl).concurrency
+  }
+
+  if (!configuration.user) {
+    configuration.user = await prompt('User: ')
+  }
+
+  if (!configuration.password) {
+    configuration.password = await prompt.password('Password: ')
+  }
+
+  return configuration
+}
+
+async function main() {
+  const kickstart = of(null as void)
+  var downloader: Downloader
+
+  try {
+    // build configuration
+    const configuration = await getConfiguration()
+    if (configuration.debug) {
+      console.log("Configuration:")
+      console.log(clone(configuration, { user: "<censored>", password: "<censored>" }))
+    }
+    const backoffConfig = {
+      initialInterval: 5000,
+      maxInterval: 60000,
+      maxRetries: configuration.retries,
+    }
+
+    // init helper for logging\debug info
+    function trace<T>(
+      logFunction: (v: T) => void,
+      extraLogFunction: (v: T) => void = v => console.log(v)
+    ) {
+      return (source: Observable<T>) => source.pipe(
+        tap(v => { logFunction(v); if (configuration.debug) extraLogFunction(v) })
+      )
+    }
+
+    // prepare downloader
+    downloader = getDownloader(configuration.courseUrl).factory(configuration)
+    await downloader.init()
+
+    // // login
+    await kickstart.pipe(
+      tap(() => console.log("Logging in...")),
+      shareReplay(),
+      flatMap(downloader.login),
+      tap(() => console.log("Logged in.")),
+      retryBackoff(backoffConfig),
+    ).toPromise()
+
+    // getting download tasks
+    const tasks = await kickstart.pipe(
+      tap(() => console.log("Getting download tasks...")),
+      shareReplay(),
+      flatMap(downloader.getDownloadTasks),
+      trace(() => {}, task => { console.log("Created download task:"); console.log(task) }),
+      distinct(task => task.id),
+      filter(task =>
+        configuration.filterRange.from <= task.index + 1
+        && configuration.filterRange.to >= task.index + 1
+      ),
+      toArray(),
+      trace(tasks => console.log(`Scheduled ${tasks.length} download tasks.`)),
+      retryBackoff(backoffConfig),
+    ).toPromise()
+
+    // perform downloads
+    const results = await kickstart.pipe(
+      tap(() => console.log("Downloading...")),
+      flatMap(() => from(tasks)),
+      shareReplay(),
+      mergeMap(
+        task => of(task).pipe(
+          trace(task => console.log(`Downloading task: ${task.name}`)),
+          shareReplay(),
+          flatMap(downloader.performDownload),
+          trace(result => console.log(`Download complete: ${result.task.name}`)),
+          retryBackoff(backoffConfig),
+        ),
+        configuration.concurrency
+      ),
+      toArray(),
+    ).toPromise()
+
+    // output report
+    downloader.reportResults(results)
+
+    // shutdown
+    await downloader.shutdown()
+    console.log("Done.")
+    process.exit(0)
+  } catch (e) {
+    console.error(e)
+    downloader?.shutdown()
+    process.exit(1)
+  }
+}
+
+main()
